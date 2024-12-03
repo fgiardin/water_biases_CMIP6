8285d647479d1ba92640c55d85c1df1b3386642d
fgiardin <67904874+fgiardin@users.noreply.github.com>
2024-12-03 18:20:37 +0100

    printed Fig. 3 with historical data

---
+++ R/process_moisture_thresholds.R
@@ -0,0 +1,143 @@
+# Function to extract and process CMIP6 data for a given model and scenario
+# to prepare them for the calculation of moisture limitation thresholds
+
+process_moisture_thresholds <- function(model_name, scenario) {
+  print(paste("Processing model:", model_name, "Scenario:", scenario))
+
+  # Initialize a list to store data for each variable
+  data_list <- list()
+
+  # Loop over each variable and load the data
+  for (variable in variables) {
+
+    # Construct file pattern to match the correct file
+    file_pattern <- paste0(variable, "_mon_", model_name, "_", scenario, "_.*_g025.nc")
+    file_list <- list.files(
+      path = paste0("data-raw/cmip6-ng/", variable, "/mon/g025/"),
+      pattern = file_pattern,
+      full.names = TRUE
+    )
+
+    if (length(file_list) == 0) {
+      warning(paste("No file found for variable", variable, "model", model_name, "and scenario", scenario))
+      return(NULL)
+    }
+
+    file_path <- file_list[1]  # Assuming there's only one file per variable, model, and scenario
+
+    # Load the data
+    data_raw <- rast(file_path)
+
+    # Filter dates
+    dates <- terra::time(data_raw)
+    selection <- which(dates >= start_date & dates <= end_date)
+    data_model <- subset(data_raw, subset = selection)
+
+    # Rotate to true coordinates (-180;180 instead of 0;360)
+    data_model <- terra::rotate(data_model)
+
+    # Focus on vegetated land
+    land_cover_raw <- rast("data-raw/landcover/landcover_MCD12C1.nc")
+    land_cover <- flip(land_cover_raw[[1]], direction = "vertical")
+    vegetated_land <- ifel(
+      land_cover == 0,
+      NA,
+      ifel(
+        land_cover == 13,
+        NA,
+        ifel(
+          land_cover > 14,
+          NA,
+          land_cover
+        )
+      )
+    )
+    vegetated_land <- resample(vegetated_land, data_model)
+    data_model <- mask(data_model, vegetated_land)
+
+    # Store the processed data
+    data_list[[variable]] <- data_model
+  }
+
+  # Check if all variables are loaded
+  if (length(data_list) != length(variables)) {
+    warning(paste("Missing variables for model", model_name, "and scenario", scenario))
+    return(NULL)
+  }
+
+  # Calculate Rn and EF using raster operations
+  Rn <- (data_list$rsds - data_list$rsus) + (data_list$rlds - data_list$rlus)
+  EF <- data_list$hfls / Rn
+
+  # Filter Rn raster to get only values where Rn > 75
+  Rn_filtered <- ifel(Rn > 75, Rn, NA)
+  EF_filtered <- mask(EF, Rn_filtered)
+  mrso_filtered <- mask(data_list$mrso, Rn_filtered)
+
+  # Convert rasters to data frames
+  df_Rn <- terra::as.data.frame(Rn_filtered, xy = TRUE)
+  df_EF <- terra::as.data.frame(EF_filtered, xy = TRUE)
+  df_mrso <- terra::as.data.frame(mrso_filtered, xy = TRUE)
+
+  # Extract dates and rename columns
+  dates <- terra::time(Rn_filtered)
+  names(df_Rn) <- c("lon", "lat", as.character(dates))
+  names(df_EF) <- c("lon", "lat", as.character(dates))
+  names(df_mrso) <- c("lon", "lat", as.character(dates))
+
+  # Pivot data frames to long format
+  df_Rn_long <- df_Rn %>%
+    data.table() %>%
+    melt(
+      measure.vars = as.character(dates),
+      variable.name = "date",
+      value.name = "Rn"
+    ) %>%
+    mutate(
+      date = lubridate::date(as.character(date)),
+      date = lubridate::floor_date(date, unit = "month")
+    ) %>%
+    drop_na()
+
+  df_EF_long <- df_EF %>%
+    data.table() %>%
+    melt(
+      measure.vars = as.character(dates),
+      variable.name = "date",
+      value.name = "EF"
+    ) %>%
+    mutate(
+      date = lubridate::date(as.character(date)),
+      date = lubridate::floor_date(date, unit = "month")
+    ) %>%
+    drop_na()
+
+  df_mrso_long <- df_mrso %>%
+    data.table() %>%
+    melt(
+      measure.vars = as.character(dates),
+      variable.name = "date",
+      value.name = "mrso"
+    ) %>%
+    mutate(
+      date = lubridate::date(as.character(date)),
+      date = lubridate::floor_date(date, unit = "month")
+    ) %>%
+    drop_na()
+
+  # Combine the data using data.table
+  result_data <- df_EF_long[
+    df_Rn_long,
+    on = .(lon, lat, date),
+    nomatch = NA
+  ][
+    df_mrso_long,
+    on = .(lon, lat, date),
+    nomatch = NA
+  ]
+
+  # Add model and scenario information
+  result_data[, `:=`(model = model_name, scenario = scenario)]
+
+  return(result_data)
+}
---
+++ analysis/1.global-maps/multi-model-means/Fig_3_hist.R
@@ -0,0 +1,250 @@
+# script to plot frequency of SM limitation (multi-model mean)
+# update to also plot comparison with historical data in a separate plot
+
+# load packages
+library(tidyverse)
+library(raster)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(sf)
+library(patchwork)
+library(cowplot)
+library(ingestr)
+library(terra)
+library(rgdal)  # for readOGR
+library(ggnewscale) # to add multiple scales in same ggplot
+library(ggpubr)
+sf_use_s2(FALSE) # switch off spherical geometry
+library(data.table)
+library(RColorBrewer)
+setwd("/Users/fgiardina/water_biases_CMIP6")
+
+# load data
+df_count_GRACE <- readRDS("data/theta_crit/monthly/df_count_GRACE.rds") %>%
+  dplyr::select(-date, -SIF, -TWS) %>%  # remove temporal values
+  unique() %>%
+  mutate(model = "Observations")
+
+df_count_mrso <- readRDS("data/theta_crit/monthly/df_count_mrso_hist.rds") %>%
+  dplyr::select(-date, -EF, -Rn, -mrso, -mrso_norm) %>%
+  unique()
+
+dt_count <- bind_rows(df_count_mrso, df_count_GRACE) %>%
+  rename(model_name = model) %>%
+  mutate(count = count*100) %>%
+  mutate(count = ifelse(Intercept < EFmax,
+                        count, # keep only rows with meaningful intercept (when the EF vs SM relationship decreases)
+                        0))   # this way 0s represent locations that are never water-limited according to our definition!
+
+# make sure all models have same cells that are NA
+# NAs = cells where the segmented function couldn't find a breakpoint (not enough data at that cell)
+setDT(dt_count) # Convert to data.table if it's not already
+na_locations <- unique(dt_count[is.na(theta_crit), .(lon, lat)]) # Identify all unique lon and lat combinations where any of the columns of interest are NA
+dt_count[na_locations, # Update the original data.table to set the values to NA for the identified lon and lat, for all models
+         on = .(lon, lat),
+         `:=` (theta_crit = NA_real_,
+               EFmax = NA_real_,
+               Slope = NA_real_,
+               Intercept = NA_real_,
+               count = NA_real_)]
+
+plot_variable <- "count"
+
+# Define breaks for the color scale (around 10 breaks given the upper_threshold)
+lower_threshold <- 0
+upper_threshold <- 100
+# interval <- upper_threshold / 9
+# rounded_interval <- round(interval, digits = 2)
+breaks <- seq(lower_threshold, upper_threshold, by = 10) # by = rounded_interval
+
+# create color palette
+colors_rdpu <- brewer.pal(9, "PuRd") # Get colors from the RdPu palette
+colors_rdpu <- colors_rdpu[2:9] # remove first color(s) as too close to white
+color_map <- colorRampPalette(colors_rdpu)(length(breaks)) # Interpolate to get the correct number of colors
+
+# download countries
+countries <- ne_countries(scale = 50, returnclass = c("sf"))
+
+coastline <- ne_coastline(scale = 50, returnclass = c("sf"))
+
+# download ocean outlines
+ocean <- ne_download(
+  scale = 50,
+  type = "ocean",
+  category = "physical",
+  returnclass = "sf")
+
+
+# Sort the models alphabetically, keeping "Observations" first
+unique_models <- unique(dt_count$model_name) # Extract unique model names
+sorted_models <- c("Observations", sort(unique_models[unique_models != "Observations"]))
+
+background_color = "white" # set the color corresponding to no data "#ECECED", # "#cad6df", "#D6D6E9"
+
+
+# Create a new (repeated for every model) column with obs data in the original dataframe
+grace_data <- dt_count %>%
+  filter(model_name == "Observations") %>%
+  dplyr::select(lon, lat, count)
+
+# calculate multi-model mean
+MMmeans <- dt_count %>%
+  filter(model_name != "Observations") %>%
+  group_by(lon, lat, scenario) %>%
+  summarise(
+    count = mean(count, na.rm = TRUE)
+  ) %>%
+  ungroup() %>%
+  mutate(model_name = "Multi-model mean") # add model name to mirror summary_deltaSM
+
+# bind rows to original dt (to add GRACE for plotting panel)
+dt_count_MMmeans <- dt_count %>%
+  bind_rows(MMmeans)
+
+summary_merged <- full_join(dt_count_MMmeans, # remove GRACE in the original dataframe to avoid repetitions
+                            grace_data, by = join_by(lon, lat),
+                            suffix = c("", "_GRACE")) # add GRACE column just to calculate comparison stats below
+
+# calculate WEIGHTS for the bias (to account for spherical coordinates)
+res <- 2.5 # resolution in degrees
+ref_grid_size <- sin(res * pi / 180) # calculate reference grid size using the sine of resolution converted to radians
+summary_merged <- summary_merged %>% # Add weights to the summary_merged dataframe
+  mutate(
+    lat1 = lat - (res / 2),  # Calculate the southern boundary of the grid cell
+    lat2 = lat + (res / 2),  # Calculate the northern boundary of the grid cell
+    gridsize = abs(sin(lat1 * pi / 180) - sin(lat2 * pi / 180)),  # Grid cell size based on sine of latitude boundaries
+    weights = gridsize / ref_grid_size  # Normalize weights by the reference grid size
+  ) %>%
+  dplyr::select(-lat1, -lat2, -gridsize)  # Clean up by removing intermediate columns
+
+
+# create the list of panels to print
+sorted_panels <- c("Observations", "Multi-model mean")
+
+# Get the list of unique scenarios
+scenarios <- unique(summary_merged$scenario)
+scenarios <- scenarios[!is.na(scenarios)]
+
+saveRDS(data, "summary_merged.rds", compress = "xz")
+
+# Loop over each scenario
+for (scene in scenarios) {
+
+  print(scene)
+  # keep filtering based on scenario outside of lapply
+  first_filter <- filter(summary_merged, scenario == scene)
+
+  # Create separate two separate panels for obs and MMMean
+  plot_list <- lapply(sorted_panels, function(model) {
+
+    print(scene)
+    if (model == "Observations") {
+      df_model <- filter(summary_merged, model_name == model)
+    } else {
+      df_model <- filter(first_filter, model_name == model)
+    }
+
+    # Calculate stats to compare models with observations
+    if (model != "Observations") {
+
+      # Calculate R squared
+      fit <- lm(count ~ count_GRACE, data = df_model)
+      r_squared <- summary(fit)$r.squared
+      r_squared_label <- bquote(italic(R)^2 == .(round(r_squared, 2)))
+
+      # Calculate absolute mean bias using weights
+      mean_bias_abs <- df_model %>%
+        filter(!is.na(count) & !is.na(count_GRACE) & !is.na(weights)) %>%
+        mutate(mean_bias_abs = abs(count - count_GRACE) * weights) %>%
+        summarise(weighted_mean_bias_abs = sum(mean_bias_abs, na.rm = TRUE) / sum(weights), na.rm = TRUE) %>%
+        pull(weighted_mean_bias_abs)
+      mean_bias_abs_label <- bquote("|Bias|" == .(round(mean_bias_abs, 0)) * "%")
+
+      # Calculate mean bias using weights
+      mean_bias <- df_model %>%
+        filter(!is.na(count) & !is.na(count_GRACE) & !is.na(weights)) %>%
+        mutate(mean_bias = (count - count_GRACE) * weights) %>%
+        summarise(weighted_mean_bias = sum(mean_bias, na.rm = TRUE) / sum(weights), na.rm = TRUE) %>%
+        pull(weighted_mean_bias)
+      mean_bias_label <- bquote("Bias" == .(round(mean_bias, 0)) * "%")
+
+      # Bias focusing on tropics
+      mean_bias_tropics <- df_model %>%
+        filter(lat >= -23 & lat <= 23) %>%
+        filter(!is.na(count) & !is.na(count_GRACE) & !is.na(weights)) %>%
+        mutate(mean_bias = (count - count_GRACE) * weights) %>%
+        summarise(weighted_mean_bias = sum(mean_bias, na.rm = TRUE) / sum(weights, na.rm = TRUE)) %>%
+        pull(weighted_mean_bias)
+      mean_bias_tropics_label <- bquote(Bias[tropics] == .(round(mean_bias_tropics, 0)) * "%")
+    }
+
+    # Plot global map
+    p <- ggplot() +
+      theme_void() +
+      geom_tile(data = df_model,
+                aes(x = lon, y = lat,
+                    color = !!sym(plot_variable), fill = !!sym(plot_variable))) +
+      geom_sf(data = ocean,
+              color = "black",
+              linetype = 'solid',
+              fill = background_color,
+              size = 1) +
+      # Color scale
+      scale_color_viridis_c(
+        option = "turbo",
+        breaks = breaks,
+        limits = c(lower_threshold, upper_threshold)
+      ) +
+      scale_fill_viridis_c(
+        option = "turbo",
+        breaks = breaks,
+        limits = c(lower_threshold, upper_threshold)
+      ) +
+      coord_sf(
+        xlim = c(-179.999, 179.999),
+        ylim = c(-60, 88),
+        expand = FALSE) +
+      theme(
+        plot.title = element_text(hjust = 0.5, size = 15),
+        legend.text = element_text(color = "black", size = 12),
+        legend.title = element_text(color = "black", size = 15),
+        plot.margin = unit(c(0, -0.3, 0.3, -0.6), 'cm'),
+        legend.key.width = unit(2.5, "cm"),
+        legend.key.height = unit(0.4, "cm"),
+        panel.border = element_rect(colour = "black", fill = NA, linewidth = 0.5),
+        panel.background = element_rect(fill = background_color, colour = NA)
+      ) +
+      guides(fill = guide_colourbar(frame.linewidth = 0.5, ticks.linewidth = 0.5, frame.colour = "black", ticks.colour = "black"),
+             color = guide_colourbar(frame.linewidth = 0.5, ticks.linewidth = 0.5, frame.colour = "black", ticks.colour = "black")) +
+      labs(
+        title = ifelse(model == "Observations", "GOME-2 SIF and GRACE observations", paste0(model, " (", scene, ")")),
+        color = "Frequency of water limitation (%)",
+        fill = "Frequency of water limitation (%)"
+      )
+
+    # Add stats for models
+    if (model != "Observations") {
+      p <- p +
+        annotate("text", x = -175, y = -11, label = r_squared_label, hjust = 0, vjust = 0, size = 4.3) + # R2
+        annotate("text", x = -175, y = -25, label = mean_bias_label, hjust = 0, vjust = 0, size = 4.3) + # Mean bias
+        annotate("text", x = -175, y = -40, label = mean_bias_tropics_label, hjust = 0, vjust = 0, size = 4.3) + # Tropics
+        annotate("text", x = -175, y = -52, label = mean_bias_abs_label, hjust = 0, vjust = 0, size = 4.3) # Absolute mean bias
+    }
+
+    return(p)
+  })
+
+  # Arrange and save the plots
+  all <- ggarrange(plotlist = plot_list,
+                   labels = "auto",
+                   ncol = 2, nrow = 1,
+                   common.legend = TRUE,
+                   legend = "bottom")
+
+  filename <- paste0("map_", plot_variable, "_", scene, "_MMmean.png")
+  ggsave(filename, path = "./", width = 12, height = 3.25, dpi = 600)
+}
+
+
+
+
